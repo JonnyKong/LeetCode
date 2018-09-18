ccd52410711af3b521a8960ba2b17c2bba12e9ff
JonnyKong <jonnykong1996@gmail.com>
2018-09-18 13:05:57 -0700

    practice

--- 231_Power_of_Two.java
+++ 231_Power_of_Two.java
@@ -1,15 +1,16 @@
 /**
- * Bit manipulation. i will eventually reach 0x80000000 < 0.
+ * Bit manipulation. 
  */
 
 class Solution {
 
     public boolean isPowerOfTwo(int n) {
-        for(int i = 1; i > 0; i <<= 1) {
-            if(n == i)
-                return true;
-        }
-        return false;
+        // for(int i = 1; i > 0; i <<= 1) {
+        //     if(n == i)
+        //         return true;
+        // }
+        // return false;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 
 }
