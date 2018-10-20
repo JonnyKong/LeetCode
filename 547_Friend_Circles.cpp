209955de9db74841b502fffe18ce5346c1782a97
JonnyKong <jonnykong1996@gmail.com>
2018-10-20 16:20:38 -0700

    practice

---
+++ 547_Friend_Circles.cpp
@@ -0,0 +1,39 @@
+/**
+ * Union find.
+ */
+
+class Solution {
+private:
+    int root(const vector<int> &id, int a) {
+        while (a != id[a])
+            a = id[a];
+        return a;
+    }
+
+    void makeUnion(vector<int> &id, int p, int q) {
+        int i = root(id, p);
+        int j = root(id, q);
+        id[i] = j;
+    }
+
+public:
+    int findCircleNum(vector<vector<int>>& a) {
+        int n = a.size();
+        vector<int> id(n);
+        for (int i = 0; i < n; ++i)
+            id[i] = i;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1 + i; j < n; ++j) {
+                if (a[i][j] == 1) {
+                    makeUnion(id, i, j);
+                }
+            }
+        }
+        set<int> id_unique;
+        for (int i : id) {
+            cout << root(id, i) << ", ";
+            id_unique.insert(root(id, i));
+        }
+        return id_unique.size();
+    }
+};
---
+++ 681_Next_Closing_Time.cpp
@@ -0,0 +1,66 @@
+/**
+ * Keep carry bit. Very delicate.
+ */
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <cstring>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+
+class Solution {
+private:
+    /* Get next element of input smaller than limit. Can wrap around */
+    int getNext(const vector<int> &v, int input, int limit) {
+        int p = (find(v.begin(), v.end(), input) - v.begin() + 1) % v.size();
+        while (v[p] >= limit)
+            p = (p + 1) % v.size();
+        return v[p];
+    }
+public:
+    string nextClosestTime(string time) {
+        set<int> s;
+        vector<int> v;
+        s.insert(time[0] - '0');
+        s.insert(time[1] - '0');
+        s.insert(time[3] - '0');
+        s.insert(time[4] - '0');
+        v.assign(s.begin(), s.end());
+
+        bool carry = 1;
+        for (int i = 4; i >= 0 && carry; --i) {
+            if (i == 2)
+                continue;
+            int cur = time[i] - '0', limit, next;
+            if (i == 4)
+                limit = 10;
+            else if (i == 3)
+                limit = 6;
+            else if (i == 1 && time[0] == '2')
+                limit = 4;
+            else if (i == 1)
+                limit = 10;
+            else
+                limit = 3;
+
+            next = getNext(v, cur, limit);
+            if (next <= cur)
+                carry = 1;
+            else
+                carry = 0;
+
+            time[i] = next + '0';
+        }
+        return time;
+    }
+};
+
+int main() {
+    string time = "23:59";
+    string time_new = Solution().nextClosestTime(time);
+    cout << time_new << endl;
+}
