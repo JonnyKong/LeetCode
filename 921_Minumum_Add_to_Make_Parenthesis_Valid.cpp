a6fb44bd820514af127fb492b637877c65141803
JonnyKong <jonnykong1996@gmail.com>
2019-02-22 21:46:48 -0800

    Just for fun

---
+++ 921_Minumum_Add_to_Make_Parenthesis_Valid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        std::stack<char> s;
+        int result = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            if (S[i] == '(') {
+                s.push('(');
+            } else {
+                if (!s.empty() &&s.top() == '(') {
+                    s.pop();
+                } else {
+                    result++;
+                }
+            }
+        }
+        result += s.size();
+        return result;
+    }
+};
