524e11007af7ac71c7278d07707197b02260eb48
JonnyKong <jonnykong1996@gmail.com>
2019-05-03 00:44:38 -0700

    practice

---
+++ 270_Closest_Binary_Search_Tree_Value.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+import (
+	"math"
+	"fmt"
+)
+
+func closestValue(root *TreeNode, target float64) int {
+	ret := root.Val
+	for root != nil {
+		if math.Abs(target - float64(root.Val)) < math.Abs(float64(ret) - target) {
+			ret = root.Val
+		}
+		if target < float64(root.Val) {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+	return ret
+}
