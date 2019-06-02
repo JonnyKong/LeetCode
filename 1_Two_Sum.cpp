54ff9eda7e96a42b1021525e9fb6fdd6d45663b6
JonnyKong <jonnykong1996@gmail.com>
2019-06-01 21:35:21 -0700

    CI: Add script

--- 1_Two_Sum.cpp
+++ 1_Two_Sum.cpp
@@ -6,6 +6,11 @@
 //
 //
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
  public:
   vector<int> twoSum(vector<int>& nums, int target) {
