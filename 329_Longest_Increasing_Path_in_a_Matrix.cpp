3f7aab2f26d384be3987e8594351334cc78088fd
JonnyKong <jonnykong1996@gmail.com>
2024-01-19 16:02:27 -0500

    Updates

--- 329_Longest_Increasing_Path_in_a_Matrix.cpp
+++ 329_Longest_Increasing_Path_in_a_Matrix.cpp
@@ -26,3 +26,58 @@ class Solution {
     return maxLength;
   }
 };
+
+
+// 2024-01-19
+class Solution {
+public:
+    vector<vector<int>> directions = {
+        {0, -1},
+        {0, 1},
+        {-1, 0},
+        {1, 0}
+    };
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return 0;
+        }
+
+        // cache[i][j] is the max possible number of moves
+        // from that loc, including current loc
+        vector<vector<int>> cache(matrix.size(), vector<int>(matrix[0].size()));
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                dfs(matrix, i, j, cache);
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                result = max(result, cache[i][j]);
+            }
+        }
+        return result;
+    }
+
+    void dfs(const vector<vector<int>>& matrix, int i, int j, vector<vector<int>> & cache) {
+        int result = 1;     // Returns number of moves possible from current loc, including current loc
+        for (auto &d : directions) {
+            int i_ = i + d[0];
+            int j_ = j + d[1];
+            if (i_ >= 0 && i_ < matrix.size() && j_ >= 0 && j_ < matrix[0].size()) {
+                if (matrix[i_][j_] > matrix[i][j]) {
+                    int result_;
+                    if (cache[i_][j_] == 0) {
+                        dfs(matrix, i_, j_, cache);
+                    }
+                    result_ = cache[i_][j_] + 1;
+                    result = max(result, result_);
+                }
+            }
+        }
+        cache[i][j] = result;
+    }
+};
