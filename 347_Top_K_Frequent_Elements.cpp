b7532466f9421624930b6e9c4e15404341d62db8
JonnyKong <jonnykong1996@gmail.com>
2025-02-18 17:26:18 -0500

    Updates

--- 347_Top_K_Frequent_Elements.cpp
+++ 347_Top_K_Frequent_Elements.cpp
@@ -35,7 +35,7 @@ bool comparator(const pair<int, int> & p1, const pair<int, int> p2) {
     return p1.second > p2.second;
 }
 
-class Solution {
+class Solution2 {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freqTable;
@@ -56,6 +56,37 @@ public:
         vector<int> ret;
         for (int i = 0; i < k; i++) {
             ret.push_back(freqVec[i].first);
+        }
+        return ret;
+    }
+};
+
+// 2025-02-18
+class Solution3 {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freqs;
+        for (auto n : nums) {
+            freqs[n] += 1;
+        }
+
+        // Pairs of <freq, num>
+        priority_queue<pair<int, int>,
+                       vector<pair<int, int>>,
+                       greater<pair<int, int>>> pq;
+        for (auto &[num, freq] : freqs) {
+            if (pq.size() == k && pq.top().first < freq) {
+                pq.pop();
+            }
+            if (pq.size() < k) {
+                pq.push({freq, num});
+            }
+        }
+
+        vector<int> ret;
+        while (!pq.empty()) {
+            ret.push_back(pq.top().second);
+            pq.pop();
         }
         return ret;
     }
